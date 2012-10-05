9bfd9fd87dfc8e7405fb45315444aa46f4de7657
Amer Khoulani <amerosdier@Amers-MacBook-Air.local>
2012-10-05 14:25:28 +0200

    committing first time from home

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 int main()
 {
+  std::cout<<"First Commit from Home"<<std::endl;
   std::cout<<"my next commit"<<std::endl;
   std::cout<<"Hello World :)"<<std::endl;
   return 0;
