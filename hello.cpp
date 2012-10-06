133997b2660ad247b2e36ce99b6bed59e14a4722
Amer Khoulani <amer.khoulani@gmail.com>
2012-10-06 10:43:46 +0200

    first commit from linux@Home

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 int main()
 {
+  std::cout<<"My first commit from Linux@home"<<std::endl;
   std::cout<<"my next commit"<<std::endl;
   std::cout<<"Hello World :)"<<std::endl;
   return 0;
