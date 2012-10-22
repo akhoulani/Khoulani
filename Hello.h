a0bd7ce019edf5502048fbb90b5a568f995f88a9
Amer Khoulani <akhoulani@gmail.com>
2012-10-22 19:39:32 +0200

    commiting

---
+++ Hello.h
@@ -0,0 +1,9 @@
+#include <iosream>
+#include <math>
+//learning about abandoning
+// Staged
+// checking editor settings
+//stage
+// learn about git diff
+// learn about git commit -v
+//staging again
